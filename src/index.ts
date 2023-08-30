fcd5f5f155a41b98afca85cb116a43fb4f964529
gustavocadev <gustavocadev@gmail.com>
2023-08-30 12:09:19 -0500

    chore: export components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,88 @@
-export { Logo } from "./components/logo/logo";
-export { Counter } from "./components/counter/counter";
+import { Tilt as Hamburger } from './components/Tilt';
+import type { JSX } from '@builder.io/qwik/jsx-runtime';
+
+import { Cross } from './components/Cross';
+import { Divide } from './components/Divide';
+import { Fade } from './components/Fade';
+import { Pivot } from './components/Pivot';
+import { Rotate } from './components/Rotate';
+import { Slant } from './components/Slant';
+import { Sling } from './components/Sling';
+import { Spin } from './components/Spin';
+import { Spiral } from './components/Spiral';
+import { Squash } from './components/Squash';
+import { Squeeze } from './components/Squeeze';
+import { Turn } from './components/Turn';
+import { Twirl } from './components/Twirl';
+
+import type {
+  CSSProperties,
+  PropFunction,
+  QRL,
+  Signal,
+} from '@builder.io/qwik';
+
+export default Hamburger;
+
+export {
+  Cross,
+  Divide,
+  Fade,
+  Pivot,
+  Rotate,
+  Slant,
+  Sling,
+  Spin,
+  Spiral,
+  Squash,
+  Squeeze,
+  Turn,
+  Twirl,
+};
+
+export interface CommonBurgerProps {
+  /** The color of the icon bars, accepts any CSS-parsable argument. */
+  color?: string;
+  /** The animation direction of the icon, left or right. */
+  direction?: 'left' | 'right';
+  /** The vertical distance between the lines. Small (sm), medium (md) or large (lg). */
+  distance?: 'sm' | 'md' | 'lg';
+  /** The duration of the animation. Can be set to zero if no animation is desired. */
+  duration?: number;
+  /** A valid `transition-timing-function` CSS value, for example 'ease-out'. */
+  easing?: string;
+  /** Hides the default browser focus style. */
+  hideOutline?: boolean;
+  /** An ARIA label to improve accessibility. */
+  label?: string;
+  /** A callback which receives a single boolean argument, indicating if the icon is toggled. */
+  onToggle$?: PropFunction<(toggled: boolean) => any>;
+  /** Specifies if the icon bars should be rounded. */
+  rounded?: boolean;
+  /** A number between 12 and 48, which sets the size of the icon. */
+  size?: number;
+  /** A way to provide your own state action. Has to be used together with a state value (the `toggled` prop). */
+  toggle?: Signal<boolean>;
+}
+
+export interface RenderOptions {
+  barHeight: number;
+  barStyles: CSSProperties;
+  burgerStyles: CSSProperties;
+  handler: QRL<() => void>;
+  isLeft: boolean;
+  isToggled: boolean;
+  label: string | undefined;
+  margin: number;
+  move: number;
+  time: number;
+  easing: string;
+  topOffset: number;
+  width: number;
+}
+
+export interface BurgerProps extends CommonBurgerProps {
+  children: (o: RenderOptions) => JSX.Element;
+  toggleInternal: Signal<boolean>;
+  lines?: number;
+}
