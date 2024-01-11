a9fd1161e6bf5dbcb5a980353e1052d589514c58
gustavocadev <gustavocadev@gmail.com>
2024-01-11 15:27:21 -0500

    chore: update example

--- src/components/example/Example.tsx
+++ src/components/example/Example.tsx
@@ -4,9 +4,15 @@ import { Rotate } from '../Rotate';
 export const Example = component$(() => {
   const isOpen = useSignal(false);
   console.log('isOpen', isOpen.value);
+
   return (
     <div>
-      <Rotate toggle={isOpen} />
+      <Rotate
+        toggle={isOpen}
+        onToggle$={(toggled) => {
+          console.log(toggled);
+        }}
+      />
       <h1>Example</h1>
     </div>
   );
